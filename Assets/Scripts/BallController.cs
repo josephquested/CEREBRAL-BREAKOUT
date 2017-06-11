b066d2f9f54714a0d50afe65099e4dbfc8bf26ae
josephquested <josephquested@gmail.com>
2017-06-12 10:09:04 +1200

    add ball controller script

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+
+	Rigidbody2D rb;
+
+	public float speed;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody2D>();
+	}
+}
