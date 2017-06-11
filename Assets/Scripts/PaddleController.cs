ddc5a36f0880ece0f1582f445607732994edbc5b
josephquested <josephquested@gmail.com>
2017-06-11 21:37:05 +1200

    add paddle controller script

---
+++ Assets/Scripts/PaddleController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleController : MonoBehaviour {
+
+	Rigidbody2D rb;
+
+	public float speed;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	void FixedUpdate ()
+	{
+		UpdateMovement();
+	}
+
+	void UpdateMovement ()
+	{
+		Vector2 movement = new Vector2(Input.GetAxisRaw("Horizontal"), 0);
+		rb.AddForce(movement * speed);
+	}
+}
