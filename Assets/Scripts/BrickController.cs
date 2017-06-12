f1f94cd4b7c549bcd3b0ce7bfba10eae4371dc4a
Joseph Quested <josephquested@gmail.com>
2017-06-13 10:17:44 +1200

    add brick controller script

---
+++ Assets/Scripts/BrickController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrickController : MonoBehaviour {
+
+	// SYSTEM //
+
+	public GameObject brickPrefab;
+
+	void Start ()
+	{
+		StartCoroutine(SpawnRoutine());
+	}
+
+	void Update ()
+	{
+
+	}
+
+	// SPAWNING //
+
+	IEnumerator SpawnRoutine ()
+	{
+		yield return null;
+	}
+}
