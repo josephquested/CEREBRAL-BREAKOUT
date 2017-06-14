727a5082bba240188c2334bb7747b5817987b33e
Joseph Quested <josephquested@gmail.com>
2017-06-14 13:55:23 +1200

    add life controller script

---
+++ Assets/Scripts/LifeController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifeController : MonoBehaviour {
+
+	// SYSTEM //
+
+	void Start () {
+
+	}
+
+	void Update ()
+	{
+
+	}
+
+	// HEARTS //
+
+	public GameObject[] heartIcons;
+
+}
