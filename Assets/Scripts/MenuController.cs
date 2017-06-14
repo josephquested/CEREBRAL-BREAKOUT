78f84c86d1189e6d9f086846ed4013a68b15bc0e
Joseph Quested <josephquested@gmail.com>
2017-06-15 10:12:25 +1200

    add menu controller and insert coin audio

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour {
+
+	// SYSTEM //
+
+	void Start ()
+	{
+
+	}
+
+	void Update ()
+	{
+		UpdateInsertCoin();
+	}
+
+	// INPUT //
+
+	void UpdateInsertCoin ()
+	{
+		if (Input.GetButtonDown("Submit"))
+		{
+			print("lets play");
+		}
+	}
+}
