756902b9dc9dfb8394d721acac765cd24b47abc8
Joseph Quested <josephquested@gmail.com>
2017-06-16 09:54:10 +1200

    add explode script

---
+++ Assets/Scripts/Explode.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explode : MonoBehaviour {
+
+	// SYSTEM //
+
+	void Start ()
+	{
+		StartCoroutine(ExplodeRoutine());
+	}
+
+	// EXPLODE //
+
+	public float lifetime;
+
+	IEnumerator ExplodeRoutine ()
+	{
+		yield return new WaitForSeconds(lifetime);
+		Destroy(gameObject);
+	}
+
+	// COLLISION //
+
+	void OnTriggerStay2D (Collider2D collider)
+	{
+		if (collider.gameObject.tag == "Brick")
+		{
+			collider.gameObject.GetComponent<Brick>().ReceiveHit();
+		}
+	}
+}
